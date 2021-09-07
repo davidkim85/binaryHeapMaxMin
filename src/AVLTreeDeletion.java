392cdf531f4a4acdd452571513fd0cb7da57371b
davidkim85 <poramok@gmail.com>
2021-09-07 23:20:41 +0300

    AVl Tree

--- src/AVLTreeDeletion.java
+++ src/AVLTreeDeletion.java
@@ -4,71 +4,58 @@ public class AVLTreeDeletion {
     Node root;
 
     // A utility function to get height of the tree
-    int height(Node N)
-    {
+    int height(Node N) {
         if (N == null)
             return 0;
         return N.height;
     }
 
     // A utility function to get maximum of two integers
-    int max(int a, int b)
-    {
+    int max(int a, int b) {
         return (a > b) ? a : b;
     }
 
     // A utility function to right rotate subtree rooted with y
     // See the diagram given above.
-    Node rightRotate(Node y)
-    {
+    Node rightRotate(Node y) {
         Node x = y.left;
         Node T2 = x.right;
-
         // Perform rotation
         x.right = y;
         y.left = T2;
-
         // Update heights
         y.height = max(height(y.left), height(y.right)) + 1;
         x.height = max(height(x.left), height(x.right)) + 1;
-
         // Return new root
         return x;
     }
 
     // A utility function to left rotate subtree rooted with x
     // See the diagram given above.
-    Node leftRotate(Node x)
-    {
+    Node leftRotate(Node x) {
         Node y = x.right;
         Node T2 = y.left;
-
         // Perform rotation
         y.left = x;
         x.right = T2;
-
         // Update heights
         x.height = max(height(x.left), height(x.right)) + 1;
         y.height = max(height(y.left), height(y.right)) + 1;
-
         // Return new root
         return y;
     }
 
     // Get Balance factor of node N
-    int getBalance(Node N)
-    {
+    int getBalance(Node N) {
         if (N == null)
             return 0;
         return height(N.left) - height(N.right);
     }
 
-    Node insert(Node node, int key)
-    {
+    Node insert(Node node, int key) {
         /* 1. Perform the normal BST rotation */
         if (node == null)
             return (new Node(key));
-
         if (key < node.key)
             node.left = insert(node.left, key);
         else if (key > node.key)
@@ -82,7 +69,7 @@ public class AVLTreeDeletion {
 
 		/* 3. Get the balance factor of this ancestor
 		node to check whether this node became
-		Wunbalanced */
+		unbalanced */
         int balance = getBalance(node);
 
         // If this node becomes unbalanced, then
